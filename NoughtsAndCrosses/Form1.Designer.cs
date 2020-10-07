68b0c4253d1df1d6c78cc1639bec1fb18915170c
billblap <65726627+billboblap@users.noreply.github.com>
2020-10-07 03:40:33 -0700

    Add files via upload

---
+++ NoughtsAndCrosses/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace NoughtsAndCrosses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.BTNreset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Location = new System.Drawing.Point(13, 13);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(50, 50);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // a2
+            // 
+            this.a2.Location = new System.Drawing.Point(69, 12);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(50, 50);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // a3
+            // 
+            this.a3.Location = new System.Drawing.Point(125, 12);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(50, 50);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(125, 68);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(50, 50);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(69, 68);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(50, 50);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(13, 69);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(50, 50);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(125, 124);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(50, 50);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(69, 124);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(50, 50);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(13, 125);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(50, 50);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.c1_Click);
+            // 
+            // BTNreset
+            // 
+            this.BTNreset.Location = new System.Drawing.Point(13, 181);
+            this.BTNreset.Name = "BTNreset";
+            this.BTNreset.Size = new System.Drawing.Size(50, 23);
+            this.BTNreset.TabIndex = 9;
+            this.BTNreset.Text = "Reset";
+            this.BTNreset.UseVisualStyleBackColor = true;
+            this.BTNreset.Click += new System.EventHandler(this.BTNreset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Bill Barg 2020";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 211);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTNreset);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Naughts and Crosses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button BTNreset;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NoughtsAndCrosses/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoughtsAndCrosses
+{
+
+    public partial class Form1 : Form
+    {
+        //grid values for each player 
+        bool a1p1 = false;
+        bool a1p2 = false;
+        bool a1p3 = false;
+        bool a1p4 = false;
+        bool a1p5 = false;
+        bool a1p6 = false;
+        bool a1p7 = false;
+        bool a1p8 = false;
+        bool a1p9 = false;
+        bool a2p1 = false;
+        bool a2p2 = false;
+        bool a2p3 = false;
+        bool a2p4 = false;
+        bool a2p5 = false;
+        bool a2p6 = false;
+        bool a2p7 = false;
+        bool a2p8 = false;
+        bool a2p9 = false;
+
+        //buttoncheck values
+        bool but1pressed = false;
+        bool but2pressed = false;
+        bool but3pressed = false;
+        bool but4pressed = false;
+        bool but5pressed = false;
+        bool but6pressed = false;
+        bool but7pressed = false;
+        bool but8pressed = false;
+        bool but9pressed = false;
+
+        //keeps track of player, f=x, t=o
+        bool playerbool = false;
+ 
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void WinCheck() 
+        {
+            if (a1p1 == true && a1p2 == true && a1p3 == true) 
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a1p4 == true && a1p5 == true && a1p6 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a1p7 == true && a1p8 == true && a1p9 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a1p1 == true && a1p4 == true && a1p7 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a1p1 == true && a1p5 == true && a1p8 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a1p3 == true && a1p6 == true && a1p9 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a1p1 == true && a1p5 == true && a1p9 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a1p3 == true && a1p5 == true && a1p7 == true)
+            {
+                MessageBox.Show("player 2 wins!");
+            }
+            else if (a2p1 == true && a2p2 == true && a2p3 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p4 == true && a2p5 == true && a2p6 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p7 == true && a2p8 == true && a2p9 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p1 == true && a2p4 == true && a2p7 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p2 == true && a2p5 == true && a2p8 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p3 == true && a2p6 == true && a2p9 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p1 == true && a2p5 == true && a2p9 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+            else if (a2p3 == true && a2p5 == true && a2p7 == true)
+            {
+                MessageBox.Show("player 1 wins!");
+            }
+
+
+
+        }
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+            if (but1pressed == false)
+            {
+                but1pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    a1.Text = ("O");
+                    playerbool = false;
+                    a1p1 = true;        
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    a1.Text = ("X");
+                    playerbool = true;
+                    a2p1 = true;
+                    WinCheck();
+                }
+            }            
+        }
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+            if (but2pressed == false)
+            {
+                but2pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    a2.Text = ("O");
+                    playerbool = false;
+                    a1p2 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    a2.Text = ("X");
+                    playerbool = true;
+                    a2p2 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+            if (but3pressed == false)
+            {
+                but3pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    a3.Text = ("O");
+                    playerbool = false;
+                    a1p3 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    a3.Text = ("X");
+                    playerbool = true;
+                    a2p3 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (but4pressed == false)
+            {
+                but4pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    b1.Text = ("O");
+                    playerbool = false;
+                    a1p4 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    b1.Text = ("X");
+                    playerbool = true;
+                    a2p4 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            if (but5pressed == false)
+            {
+                but5pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    b2.Text = ("O");
+                    playerbool = false;
+                    a1p5 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    b2.Text = ("X");
+                    playerbool = true;
+                    a2p5 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            if (but6pressed == false)
+            {
+                but6pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    b3.Text = ("O");
+                    playerbool = false;
+                    a1p6 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    b3.Text = ("X");
+                    playerbool = true;
+                    a2p6 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void c1_Click(object sender, EventArgs e)
+        {
+            if (but7pressed == false)
+            {
+                but7pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    c1.Text = ("O");
+                    playerbool = false;
+                    a1p7 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    c1.Text = ("X");
+                    playerbool = true;
+                    a2p7 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+            if (but8pressed == false)
+            {
+                but8pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    c2.Text = ("O");
+                    playerbool = false;
+                    a1p8 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    c2.Text = ("X");
+                    playerbool = true;
+                    a2p8 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void c3_Click(object sender, EventArgs e)
+        {
+            if (but9pressed == false)
+            {
+                but9pressed = true;
+                if (playerbool == true)
+                {
+                    //place an o
+                    c3.Text = ("O");
+                    playerbool = false;
+                    a1p9 = true;
+                    WinCheck();
+                }
+                else if (playerbool == false)
+                {
+                    //place an x
+                    c3.Text = ("X");
+                    playerbool = true;
+                    a2p9 = true;
+                    WinCheck();
+                }
+            }
+        }
+
+        private void BTNreset_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
